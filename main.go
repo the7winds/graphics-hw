cbe54321f8f2171db01a888869b77545676065f8
Gleb Valin <the7winds@yandex.ru>
2017-12-02 19:49:08 +0300

    fix versioning

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func (app *Application) glfwInit() error {
 
 	glfw.WindowHint(glfw.Resizable, glfw.False)
 	glfw.WindowHint(glfw.ContextVersionMajor, 4)
-	glfw.WindowHint(glfw.ContextVersionMinor, 5)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
 	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
 	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
 
