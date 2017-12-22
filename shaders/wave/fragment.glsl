a3134e40052535470a5bf487179f6c59f022735d
Gleb Valin <the7winds@yandex.ru>
2017-12-22 20:27:19 +0300

    make wave longer

--- shaders/wave/fragment.glsl
+++ shaders/wave/fragment.glsl
@@ -10,7 +10,7 @@ void main() {
     float r  = length(pos);
     float d  = rt - r;
 
-    float v = float(abs(d) < 0.01);
+    float v = float(abs(d) < 0.05);
 
-    waveHeight = v * pow(2.7, -30 * r) * sin(314 * d);
+    waveHeight = v * pow(2.7, -30 * r) * sin(62.8 * d);
 }
